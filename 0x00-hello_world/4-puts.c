04d265cc7bdf71b6eaccc71e95e06540d2538e76
will-me-me <willykaria777@gmail.com>
2023-06-15 05:50:51 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* this function print a specified message using pus */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle\n");
    	return 0;
 } 
