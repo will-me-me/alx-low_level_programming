54f59d00275bebda316064fda59e56564a4b0e82
will-me-me <willykaria777@gmail.com>
2023-06-15 06:11:07 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - This the main entry point of our funcction
- * the fuction prints a specifiv message and 
+ * main - This is the main entry point of our function
+ * The function prints a specific message
  *
- * Return: 0 if succesful
+ * Return: 0 if successful
  */
-
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
