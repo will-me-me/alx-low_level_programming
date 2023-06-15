f2dbe1c9ce9185a86eca3e0c17fe0010eb61cfac
will-me-me <willykaria777@gmail.com>
2023-06-15 06:00:45 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,14 @@
  *
  * Return: 0 if succesful
  */
+
 int main(void)
+
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+
+    puts("\"Programming is like building a multilingual puzzle");
+
     return (0);
+
 }
 
