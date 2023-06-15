6f352e666c1366baaee53bdd617283b040777105
will-me-me <willykaria777@gmail.com>
2023-06-15 10:41:49 -0700

    updated the file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This is the main entry point of our function
+ * main -The entry point
  * The function prints the size of various types on the computer it is compiled 
  *
  * Return: 0 if successful
@@ -10,12 +10,10 @@
 int main(void)
 
 {
-
 printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
 printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
 printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(long));
 printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(long long));
 printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
 return 0;
-
 }
