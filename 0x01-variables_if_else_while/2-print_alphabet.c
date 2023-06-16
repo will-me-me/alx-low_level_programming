113940ff889ab4d8275040e2baac4337f5d18474
will-me-me <willykaria777@gmail.com>
2023-06-16 09:01:29 -0700

    the last digit check

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase using putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* initialize chr */
+
+	char  alphabetic_letter;
+
+	/* a for loop to to loop from a to z */
+	for (alphabetic_letter = 'a'; alphabetic_letter <= 'z'; alphabetic_letter++)
+		putchar(alphabetic_letter)
+
+	putchar('\n')
+
+	return (0)
+
+}
