8e62325c2461c0aca069f56e4678409481fd9db9
will-me-me <willykaria777@gmail.com>
2023-06-16 11:56:00 -0700

    printing nums from 1 to 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,18 +10,13 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		putchar(i + '0');
+	}
 	putchar('\n');
 
 	return (0);
 }
-
