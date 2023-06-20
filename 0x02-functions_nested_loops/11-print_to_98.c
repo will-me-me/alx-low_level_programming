34c79b9a76f198b4eb23a7e811fc6faf1731d2f8
will-me-me <willykaria777@gmail.com>
2023-06-20 10:25:29 -0700

    printing numbers that range from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,28 @@ void print_to_98(int n)
     {
         for (; n <= 98; n++)
         {
-            if (n < 10)
-                _putchar('0' + n);
+            if (n < 0)
+            {
+                _putchar('-');
+                if (n > -10)
+                    _putchar('0' - n);
+                else
+                {
+                    _putchar('0' - (n / 10));
+                    _putchar('0' - (n % 10));
+                }
+            }
             else
             {
-                _putchar('0' + (n / 10));
-                _putchar('0' + (n % 10));
+                if (n < 10)
+                    _putchar('0' + n);
+                else
+                {
+                    _putchar('0' + (n / 10));
+                    _putchar('0' + (n % 10));
+                }
             }
+            
             if (n != 98)
             {
                 _putchar(',');
@@ -28,13 +43,28 @@ void print_to_98(int n)
     {
         for (; n >= 98; n--)
         {
-            if (n < 10)
-                _putchar('0' + n);
+            if (n < 0)
+            {
+                _putchar('-');
+                if (n > -10)
+                    _putchar('0' - n);
+                else
+                {
+                    _putchar('0' - (n / 10));
+                    _putchar('0' - (n % 10));
+                }
+            }
             else
             {
-                _putchar('0' + (n / 10));
-                _putchar('0' + (n % 10));
+                if (n < 10)
+                    _putchar('0' + n);
+                else
+                {
+                    _putchar('0' + (n / 10));
+                    _putchar('0' + (n % 10));
+                }
             }
+            
             if (n != 98)
             {
                 _putchar(',');
@@ -44,4 +74,3 @@ void print_to_98(int n)
     }
     _putchar('\n');
 }
-
