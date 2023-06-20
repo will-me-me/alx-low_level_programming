2eda1a8f44594e242d956a842a0d246c6cbdd9b4
will-me-me <willykaria777@gmail.com>
2023-06-20 10:17:21 -0700

    printing numbers that range from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,38 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98.
- * @n: The starting number.
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at n
  */
-
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (; n <= 98; n++)
+		while (n > 98)
 		{
-			_putchar("%d", n);
-			if (n != 98)
-				_putchar(", ");
+			_putchar('0' + (n / 10));
+            		_putchar('0' + (n % 10));
+           		_putchar(',');
+            		_putchar(' ');
+            		n--;	
 		}
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+		_putchar('\n');
 	}
 	else 
 	{
-		for (; n >= 98; n--)
+		while (n < 98 )
 		{
-			_putchar("%d", n);
-			if (n != 98)
-				_putchar(", ");
+			_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
+			_putchar(',');
+			_putchar(' ');
+			n++
 		}
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
