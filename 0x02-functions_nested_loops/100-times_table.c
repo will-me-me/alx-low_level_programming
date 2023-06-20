48b19dfd7f27058658c34cbeac34365f27ae3ed0
will-me-me <willykaria777@gmail.com>
2023-06-20 13:55:41 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,34 +4,42 @@
  * print_times_table - Prints the n times table, starting with 0
  * @n: The number to generate the times table for
  */
+void print_number(int n)
+{
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number to generate the times table for
+ */
 void print_times_table(int n)
 {
 	int i, j;
 
 	if (n > 15 || n < 0)
 		return;
+
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			int res = i * j;
+			int result = i * j;
 			if (j > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (res < 100 )
+				if (result < 100)
 					_putchar(' ');
-				if (res < 10)
+				if (result < 10)
 					_putchar(' ');
 			}
-			if (res < 10)
-				_putchar(res + '0');
-			else
-			{
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
+			print_number(result);
 		}
 		_putchar('\n');
 	}
 }
+
