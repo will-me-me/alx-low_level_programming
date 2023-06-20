ce9673ab9a5857464921a5f5e50428123a510663
will-me-me <willykaria777@gmail.com>
2023-06-20 15:13:27 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, separated by comma and space.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count;
+	unsigned int num1 = 1, num2 = 2, nextTerm;
+
+	printf("%u, %u", num1, num2);
+	
+	for (count = 3; count <= 98; count++)
+	{
+		nextTerm = num1 + num2;
+		printf(", %u", nextTerm);
+		num1 = num2;
+		num2 = nextTerm;
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
