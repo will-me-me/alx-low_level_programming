2cc556b6a4c261133a4e310c2fb2a74e7f218f45
will-me-me <willykaria777@gmail.com>
2023-06-20 14:31:16 -0700

    computing sum of all multiples of 4 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Computes and prints the sum of all 
+ * Description: Computes and prints the sum of all
  * the multiples of 3 or 5 below 1024.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int sum = 0;
