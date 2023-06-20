18fa2660e5ef96efc653812428949b172bd87e7f
will-me-me <willykaria777@gmail.com>
2023-06-20 09:45:28 -0700

    multiplication table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,28 +6,26 @@
 
 void times_table(void)
 {
-	int i, j, product;
+	int i, j, prod;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			product = i * j;
+		_putchar('0');
 
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			
-			if (product < 10)
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = i * j;
+
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((product / 10) + '0');
-			
-			_putchar((product % 10) + '0');
-		}
+				_putchar((prod / 10) + '0');
 
+			_putchar((prod % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
