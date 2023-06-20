e173140a314e6a1b840eb7aada28b3f1fe4e1b79
will-me-me <willykaria777@gmail.com>
2023-06-20 10:19:42 -0700

    printing numbers that range from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,47 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers from input to 98,
- *               in order separated by a comma followed by a space.
- * @n: The number to begin counting at n
+ * print_to_98 - Prints all natural numbers from n to 98.
+ * @n: The starting number.
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-		{
-			_putchar('0' + (n / 10));
-            		_putchar('0' + (n % 10));
-           		_putchar(',');
-            		_putchar(' ');
-            		n--;	
-		}
-		_putchar('0' + (n / 10));
-		_putchar('0' + (n % 10));
-		_putchar('\n');
-	}
-	else 
-	{
-		while (n < 98 )
-		{
-			_putchar('0' + (n / 10));
-			_putchar('0' + (n % 10));
-			_putchar(',');
-			_putchar(' ');
-			n++
-		}
-		_putchar('0' + (n / 10));
-		_putchar('0' + (n % 10));
-		_putchar('\n');
-	}
+    if (n <= 98)
+    {
+        for (; n <= 98; n++)
+        {
+            if (n < 10)
+                _putchar('0' + n);
+            else
+            {
+                _putchar('0' + (n / 10));
+                _putchar('0' + (n % 10));
+            }
+            if (n != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    else
+    {
+        for (; n >= 98; n--)
+        {
+            if (n < 10)
+                _putchar('0' + n);
+            else
+            {
+                _putchar('0' + (n / 10));
+                _putchar('0' + (n % 10));
+            }
+            if (n != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    _putchar('\n');
 }
+
