b603f8a780bc44f8a46539479a52f9ab63bcfd18
will-me-me <willykaria777@gmail.com>
2023-06-20 15:25:50 -0700

    Fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,9 +16,10 @@ int main(void)
 
 	printf("%u, %u", num1, num2);
 	
-	for (count = 3; count <= 96; count++)
+	for (count = 3; count <= 98; count++)
 	{
 		unsigned int nextTerm = num1 + num2;
+
 		printf(", %u", nextTerm);
 
 
