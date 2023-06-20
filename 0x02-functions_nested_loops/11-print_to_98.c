1c554f162a8d851cc0af182ef56f258522926c34
will-me-me <willykaria777@gmail.com>
2023-06-20 13:30:08 -0700

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,71 +6,36 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            if (n < 0)
-            {
-                _putchar('-');
-                if (n > -10)
-                    _putchar('0' - n);
-                else
-                {
-                    _putchar('0' - (n / 10));
-                    _putchar('0' - (n % 10));
-                }
-            }
-            else
-            {
-                if (n < 10)
-                    _putchar('0' + n);
-                else
-                {
-                    _putchar('0' + (n / 10));
-                    _putchar('0' + (n % 10));
-                }
-            }
-            
-            if (n != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            if (n < 0)
-            {
-                _putchar('-');
-                if (n > -10)
-                    _putchar('0' - n);
-                else
-                {
-                    _putchar('0' - (n / 10));
-                    _putchar('0' - (n % 10));
-                }
-            }
-            else
-            {
-                if (n < 10)
-                    _putchar('0' + n);
-                else
-                {
-                    _putchar('0' + (n / 10));
-                    _putchar('0' + (n % 10));
-                }
-            }
-            
-            if (n != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    _putchar('\n');
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			putchar((n / 100) + '0');
+			putchar(((n / 10) % 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(',');
+			putchar(' ');
+			n--;
+		}
+		putchar((n / 100) + '0');
+		putchar(((n / 10) % 10) + '0');
+		putchar((n % 10) + '0');
+		putchar('\n');
+	}
+	else
+	{
+		while (n < 98)
+		{
+			putchar((n / 100) + '0');
+			putchar(((n / 10) % 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(',');
+			putchar(' ');
+			n++;
+		}
+		putchar((n / 100) + '0');
+		putchar(((n / 10) % 10) + '0');
+		putchar((n % 10) + '0');
+		putchar('\n');
+	}
 }
