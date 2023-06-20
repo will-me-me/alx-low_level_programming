4db6232aee4ecbc1ae022568e779d1c1c6f9e56f
will-me-me <willykaria777@gmail.com>
2023-06-20 08:43:48 -0700

    printingg  the absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - Prints the sign of a number.
  * @n: The number to be checked.
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero.
+ * Return: 1 if n is greater than zero,
+ * 0 if n is zero,
+ * -1 if n is less than zero.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -19,7 +21,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
