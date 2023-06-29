4acb04cb848eeb8326790e26baf2a32a5dbc0548
will-me-me <willykaria777@gmail.com>
2023-06-29 15:35:03 -0700

    updated he magiic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,6 +14,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
 	 * Write your line of code here...
 	 * Remember:
@@ -22,7 +23,9 @@ int main(void)
 	 * - Only one statement is allowed
 	 * - You are not allowed to code anything else than this line of code
 	 */
+
 	*(p + 5) = 98;
+
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
