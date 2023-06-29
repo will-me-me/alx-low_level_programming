2069c9b10be10f7540a76a1ea41bdaf809e3601c
will-me-me <willykaria777@gmail.com>
2023-06-29 12:57:45 -0700

    added a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the value of a[2] after assigning it 98
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int i;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &i;
+	a[2] = 1024;
+	p = &n;
 
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
+	/**
+	 * Write your line of code here...
+	 * Remember:
+	 * - You are not allowed to use 'a'
+	 * - You are not allowed to modify 'p'
+	 * - Only one statement is allowed
+	 * - You are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
 
-  *(p + 5) = 98;
-  /* ...so that this prints 98\i */
-  printf("a[2] = %d\i", a[2]);
-  return (0);
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
+
