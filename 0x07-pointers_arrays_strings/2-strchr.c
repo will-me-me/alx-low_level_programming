a05dba7dee81fde728d15bbe50490bf4ff1906c0
will-me-me <willykaria777@gmail.com>
2023-07-03 08:24:01 -0700

    corrected the error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,14 +2,14 @@
 
 /**
  * _strchr - locates a character in a string
- * @s: pointer to string
- * @c: character to locate
+ * @s: string to search
+ * @c: character to search for
  *
- * Return: pointer to first occurrence of c in s, or NULL if not found
+ * Return: pointer to first occurrence of c, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 			return (s);
