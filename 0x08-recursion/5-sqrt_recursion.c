0be8583f7155bf9eb64620b5236d7a260819002a
will-me-me <willykaria777@gmail.com>
2023-07-05 08:30:02 -0700

    printing the squre root of a given number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to find the square root of.
+ *
+ * Return: the square root of n.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	return (1 + _sqrt_recursion(n - 1));
+}
+
