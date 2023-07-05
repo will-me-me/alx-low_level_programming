3b331b2fa7ef8b953ee3facec62650098be60419
will-me-me <willykaria777@gmail.com>
2023-07-05 08:36:45 -0700

    printing the squre root of a given number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,8 @@
-#include "main.h"
-
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: number to find the square root of.
  *
- * Return: the square root of n.
+ * Return: the square root of n, or -1 if n does not have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
@@ -12,10 +10,42 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	if (n == 0)
+
+	if (n == 0 || n == 1)
 	{
-		return (0);
+		return (n);
 	}
-	return (1 + _sqrt_recursion(n - 1));
+
+	return (sqrt_helper(n, 1, n));
+}
+
+/**
+ * sqrt_helper - recursive helper function to find the square root of a number.
+ * @n: number to find the square root of.
+ * @start: starting point for the search.
+ * @end: ending point for the search.
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root.
+ */
+int sqrt_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+
+	if (mid * mid > n)
+	{
+		return (sqrt_helper(n, start, mid - 1));
+	}
+
+	return (sqrt_helper(n, mid + 1, end));
 }
 
