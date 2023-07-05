a57566c0c4a24c5f5a10cfe32814a4b1d3925b84
will-me-me <willykaria777@gmail.com>
2023-07-05 08:38:48 -0700

    printing the squre root of a given number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n)
 		return (n);
 	}
 
-	return (sqrt_helper(n, 1, n));
+	return sqrt_helper(n, 1, n);
 }
 
 /**
@@ -46,6 +46,6 @@ int sqrt_helper(int n, int start, int end)
 		return (sqrt_helper(n, start, mid - 1));
 	}
 
-	return (sqrt_helper(n, mid + 1, end));
+	return sqrt_helper(n, mid + 1, end);
 }
 
