493bfeb4d5f22f5459dcfa0aaea475a1304d1aa3
will-me-me <willykaria777@gmail.com>
2023-07-07 11:55:34 -0700

    updated the main.h file

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
-Static libraries
+Static libraries 
+a static file refers to a file that is restricted to a single translation unit and cannot be accessed or modified by other translation units.
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+int _putchar(char c);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
@@ -19,5 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _abs(int n);
+
 #endif
