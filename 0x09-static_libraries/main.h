0dc33511f7968b7c3240f20162d98250742800df
will-me-me <willykaria777@gmail.com>
2023-07-07 11:14:33 -0700

    change the main.h file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,7 +3,6 @@
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
@@ -20,5 +19,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+int _abs(int n);
 #endif
