8148d6fc2740b506aae91bc6e192bce13cf55fe8
will-me-me <willykaria777@gmail.com>
2023-07-13 14:34:18 -0700

    updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errorExit(void);
-int multiplyNumbers(int num1, int num2);
-int isNumber(const char *str);
-int main(int argc, char *argv[]);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
