744ca59bc3b2f8481129e143fe3916083eafabb0
will-me-me <willykaria777@gmail.com>
2023-07-13 14:19:16 -0700

    updated the main file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errorExit(void);
+int multiplyNumbers(int num1, int num2);
+int isNumber(const char *str);
+int main(int argc, char *argv[]);
 
 #endif
