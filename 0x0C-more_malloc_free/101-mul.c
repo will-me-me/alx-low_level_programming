686f44f1e638132aba115705f139400c3ce208fe
will-me-me <willykaria777@gmail.com>
2023-07-13 13:34:25 -0700

    added more code on the 1001-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,43 +1,20 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * ExitfromError - Exits from error
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: void
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void ExitfromError(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * multiplyNumbers - Multiplies two numbers
- * @num1: First number
- * @num2: Second number
- *
- * Return: Result of multiplication
- */
-int multiplyNumbers(int num1, int num2)
-{
-	return (num1 * num2);
-}
-
-/**
- * isNumber - Checks if a string is a number
- * @str: String to check
- *
- * Return: 1 if true, 0 if false
- */
-int isNumber(const char *str)
+int is_digit(char *s)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i])
 	{
-		if (str[i] < '0' || str[i] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
 	}
@@ -45,27 +22,78 @@ int isNumber(const char *str)
 }
 
 /**
- * main - Multiplies two numbers
- * @argc: Number of arguments
- * @argv: Array of arguments
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: 0 if success, 98 if failure
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
-		ExitfromError();
-	if (!isNumber(argv[1]) || !isNumber(argv[2]))
-		ExitfromError();
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	result = multiplyNumbers(num1, num2);
-	printf("%d\n", result);
-
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
-
