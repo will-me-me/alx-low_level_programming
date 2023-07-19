57b8c833fa9b991ba5737d976325cea83a7afee5
will-me-me <willykaria777@gmail.com>
2023-07-19 08:40:31 -0700

    prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (SUCCESS)
+ */
+
+
+int main(int argc, char **argv)
+{
+	int num, k = 0, (*code)(int, char **) = main;
+
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		exit(1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		puts("Error");
+		exit(2);
+	}
+
+	while (k < num)
+	{
+		opcode = *(unsigned char *)code;
+		printf("%.2x", opcode);
+		if (k < (num - 1))
+		{
+			printf(" ");
+		}
+
+		code++;
+		k++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
