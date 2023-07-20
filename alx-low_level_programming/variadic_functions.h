3cf6440059defe53212c2941179f39e284e46da6
will-me-me <willykaria777@gmail.com>
2023-07-20 02:22:12 -0700

    printing numbers followed by a new line

--- alx-low_level_programming/variadic_functions.h
+++ alx-low_level_programming/variadic_functions.h
@@ -1,18 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/**
- * struct print - Struct print
- *
- * @type: The format
- * @f: The function associated
- */
-typedef struct print
-{
-	char *type;
-	void (*f)(va_list);
-} print_t;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
