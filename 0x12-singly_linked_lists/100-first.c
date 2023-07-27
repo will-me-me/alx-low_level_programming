954c0110bf78754f7703e3d2479430c83aaa5436
will-me-me <willykaria777@gmail.com>
2023-07-27 11:37:35 -0700

    updated the lists.h file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,11 @@
-#include "main.h"
+#include "lists.h"
 
 /**
- * print_first - Function to be executed before main
- * Prints a message before the main function is executed
+ * con_print - prints before executing main
  */
-void print_first(void) __attribute__ ((constructor));
 
-/**
- * print_first - Function to be executed before main
- * Prints a message before the main function is executed
- */
-void print_first(void)
+void con_print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!");
-printf("\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
