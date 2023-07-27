cdaf9cd2baeadcc3b37dd0f99f5244be09664a06
will-me-me <willykaria777@gmail.com>
2023-07-27 02:12:14 -0700

    printing str before main func

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * print_first - Function to be executed before main
  * Prints a message before the main function is executed
  */
-void print_first(void) __attribute__ ((constructor));
+/*void print_first(void) __attribute__ ((constructor));*/
 
 /**
  * print_first - Function to be executed before main
@@ -12,6 +12,7 @@ void print_first(void) __attribute__ ((constructor));
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!");
+	printf("\n");
 }
 
