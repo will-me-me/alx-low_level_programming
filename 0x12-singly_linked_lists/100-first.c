d0a43580343601fe6ed9d13a496cb5041d5a8be2
will-me-me <willykaria777@gmail.com>
2023-07-27 02:35:05 -0700

    updated the 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,7 +12,6 @@ void print_first(void) __attribute__ ((constructor));
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!");
+printf("\n");
 }
-
