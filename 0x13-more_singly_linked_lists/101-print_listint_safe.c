443494ce7ba1a0c9dcacc0a5b13927c17b7fde11
will-me-me <willykaria777@gmail.com>
2023-08-01 13:38:26 -0700

    updated the linked list print

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints all nodes of a linkedlist
+ * @head: head of the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t len = 0;
+
+	while (head != NULL)
+	{
+		len++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head > head->next)
+			head = head->next;
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			return (len);
+		}
+	}
+
+	return (len);
+}
