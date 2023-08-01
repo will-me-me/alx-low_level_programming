eb78698bd42c502b33aa6dd5ca317bda5bc0f74d
will-me-me <willykaria777@gmail.com>
2023-08-01 01:43:24 -0700

    fix but in task two

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,35 +1,36 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - add a node at the end of the list
- * @head: pointer to the first node in the list
- * @n: data to be added in the new node
+ * add_nodeint_end - add list to end of node
+ * @head: pointer to head of list
+ * @n: data in new node
  *
- * Return: pointer to new node(SUCCESS)
- * NULL (FAIL)
+ * Return: pointer to head of list
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newnode;
-	listint_t *temp = *head;
+	listint_t *p;
+	listint_t *lastNode = *head;
 
-	newnode = malloc(sizeof(listint_t));
+	p = malloc(sizeof(listint_t));
+	if (p == NULL)
+		return (0);
 
-	if (!newnode)
-		return (NULL);
-
-	newnode->n = n;
-	newnose->next = NULL;
+	p->n = n;
+	p->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = newnode;
+		*head = p;
 		return (*head);
 	}
-			while (temp->next)
-			temp = temp->next;
 
-			temp->next = newnode;
-			return (*head);
-			}
+	while (lastNode->next != NULL)
+	{
+		lastNode = lastNode->next;
+	}
+	lastNode->next = p;
+
+	return (*head);
+}
