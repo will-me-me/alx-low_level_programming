28460c8150ff170910e03bb6214bad38e3cb0d00
will-me-me <79546675+will-me-me@users.noreply.github.com>
2024-02-07 22:59:38 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,7 +48,6 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
